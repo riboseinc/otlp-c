b921b84c8dec22f888acc0a88279c451b6872500
Ribose Engineering <engineering@ribose.com>
2026-08-09 08:20:52 +0800

    chore: bump version to 0.5.26 + TODO 66

--- include/otlp-c/version.h
+++ include/otlp-c/version.h
@@ -23,8 +23,8 @@ extern "C" {
 
 #define OTLP_C_VERSION_MAJOR 0
 #define OTLP_C_VERSION_MINOR 5
-#define OTLP_C_VERSION_PATCH 25
-#define OTLP_C_VERSION_STRING "0.5.25"
+#define OTLP_C_VERSION_PATCH 26
+#define OTLP_C_VERSION_STRING "0.5.26"
 
 #define OTLP_C_VERSION                                                            \
 	(((uint32_t)OTLP_C_VERSION_MAJOR << 24) |                                 \
