2273e6dd512139928f160162b01b23809f133655
Ribose Engineering <engineering@ribose.com>
2026-08-10 11:51:30 +0800

    Bump 0.5.38 -> 0.5.39
    Version, changelog, and TODO file for the buf-init-check +
    bench-batch release.

--- include/otlp-c/version.h
+++ include/otlp-c/version.h
@@ -23,8 +23,8 @@ extern "C" {
 
 #define OTLP_C_VERSION_MAJOR 0
 #define OTLP_C_VERSION_MINOR 5
-#define OTLP_C_VERSION_PATCH 38
-#define OTLP_C_VERSION_STRING "0.5.38"
+#define OTLP_C_VERSION_PATCH 39
+#define OTLP_C_VERSION_STRING "0.5.39"
 
 #define OTLP_C_VERSION                                                            \
 	(((uint32_t)OTLP_C_VERSION_MAJOR << 24) |                                 \
