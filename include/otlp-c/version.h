9c0cc12a50b8b29d8babd29862fc15dd51fc5dab
Ribose Engineering <engineering@ribose.com>
2026-08-13 11:09:20 +0800

    Bump 0.5.55 -> 0.5.56

--- include/otlp-c/version.h
+++ include/otlp-c/version.h
@@ -23,8 +23,8 @@ extern "C" {
 
 #define OTLP_C_VERSION_MAJOR 0
 #define OTLP_C_VERSION_MINOR 5
-#define OTLP_C_VERSION_PATCH 55
-#define OTLP_C_VERSION_STRING "0.5.55"
+#define OTLP_C_VERSION_PATCH 56
+#define OTLP_C_VERSION_STRING "0.5.56"
 
 #define OTLP_C_VERSION                                                            \
 	(((uint32_t)OTLP_C_VERSION_MAJOR << 24) |                                 \
